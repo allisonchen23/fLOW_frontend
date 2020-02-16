4a7043051fcca3510d73bff257ee5eebfce76441
Raeka Lin <raelin@s-169-232-111-239.resnet.ucla.edu>
2020-02-16 15:12:50 -0800

    displayed data from firebase onto a graph

--- src/graphdata/Dashboard.js
+++ src/graphdata/Dashboard.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import classes from "./Dashboard.module.css";
 import LineGraph from "./myLineGraph";
 import chartIcon from "../assets/svg/chart-icon.svg";
-import { volWater, dayLabels } from "./mockData";
+import { dayLabels } from "./mockData";
+import firebase from "../firebase"
+
 
 export default class Dashboard extends Component {
-    state = {
-        data: volWater,
-        labels: dayLabels
+    constructor() {
+        super();
+        this.state = {
+          data: [],
+          labels: dayLabels,
+        };
+      }
+
+    componentDidMount = () => {
+        let db = firebase.database();
+        let dataRef = db.ref('graph_dummy');
+        var volume = [];
+        dataRef.on('value', snapshot =>{
+          const data = snapshot;
+          data.forEach(childSnapshot => {
+            volume.push(childSnapshot.child('volume').val());
+          })
+          this.setState({ data: volume })
+        })
     }
 
     render() {
