c5c6b40e131d5d99f80b69aa52d4b645e8c787f0
Sammi Owyang <owyang.samantha@gmail.com>
2020-02-25 23:24:37 -0800

    more tips

--- src/pages/tips.js
+++ src/pages/tips.js
@@ -7,6 +7,7 @@ import './globalstyle.css';
 import logo from '../assets/svg/flow_logo.svg';
 import dbman from '../assets/svg/dashboardimg.svg';
 import drop from '../assets/svg/drop.svg';
+import waterimage from '../assets/jpeg/waterimage.jpg';
 
 // Imports icons
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -31,6 +32,20 @@ class Welcome extends Component {
   }
 }
 
+class TipsMessage extends Component {
+
+  getMsg() {
+    var msg = ['Showering, bathing and using the toilet account for about two-thirds of the average family’s water usage.', "Every drop has an impact.", "What can you do to save our planet?"];
+    var i = Math.floor(Math.random() * Math.floor(3));
+    return msg[i];
+  }
+
+  render() {
+    return <h3 className="box_text blue_box_text">{this.getMsg()}</h3>;
+  }
+}
+
+
 console.log(logo);
 
 class Tips extends Component { 
@@ -39,29 +54,30 @@ class Tips extends Component {
         <React.Fragment>
           <div className="body">
             <div className="body_wrap">
-              <div className="body_container_t">
-                <div className="body_box" id="message_box_t">
+              <div className="body_container">
+                <div className="body_box_t long2_t double_height_t">
+                  <div className="box_content_t" id="first_image">
+                    {/* <img className="dropimg" src={drop}/>              */}
+                  </div>  
+                </div> 
+                <div className="body_box_t long2_t double_height_t" id="message_box_t">
                   <div className="box_content_t">
                     <Welcome />
                   </div>  
                 </div> 
-                <div className="body_box_t long_t">
+                <div className="body_box_t full_width_t double_height_t" id="message_box3_t">
                   <div className="box_content_t">
-                    <img className="dropimg" src={drop}/> 
-                                  
+                  <h2 className="box_text white_box_text"> Cutting 4 minutes from your shower can save 30 gallons of water. </h2>
                   </div>  
                 </div> 
-                <div className="body_box_t full_width_t" id="message_box3_t">
+                <div className="body_box_t long3_t triple_height_t" id="message_box5_t">
                   <div className="box_content_t">
-                  <h2 className="box_text white_box_text"> hello </h2>
+                    <img className="dropimg" src={drop}/>            
+                    <TipsMessage />
+                    {/* <h3 className="box_text blue_box_text">Showering, bathing and using the toilet account for about two-thirds of the average family’s water usage.</h3>  */}
                   </div>  
                 </div> 
-                <div className="body_box long_t" id="message_box5_t">
-                  <div className="box_content_t">
-                    <h2 className="box_text blue_box_text">hi</h2> 
-                  </div>  
-                </div> 
-                <div className="body_box_t">
+                <div className="body_box_t double_height_t">
                   <div className="box_content_t">
                   <h2 className="box_text white_box_text">normal box</h2>
                   </div>  
