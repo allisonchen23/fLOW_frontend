be26fc402ddeebc730cd983acf221f59bddf0982
Ray Huang <rayhuang.cj@gmail.com>
2020-03-01 20:53:04 -0800

    Combined both Raeka's and Ray's codes
    Kept the doc separated (need to clean the codes up and set start day as the last seven days)

--- src/graphdata/Dashboard.js
+++ src/graphdata/Dashboard.js
@@ -2,37 +2,53 @@ import React, { Component } from 'react';
 import classes from "./Dashboard.module.css";
 import LineGraph from "./myLineGraph";
 //import { dayLabels } from "./mockData";
-import firebase from "../firebase"
 
 let dayLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let monthLabels = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
 
 export default class Dashboard extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            // start from 2/10/20
-          daily_sums: {1581336000000: 30, 1581422400000: 54, 1581508800000: 100, 1581595200000: 14, 1581681600000: 93, 1581768000000: 24, 1581854400000: 48, 1581940800000: 33, 1582027200000: 5},
+          // start from 2/10/20
+          //daily_sums: {1581336000000: 30, 1581422400000: 54, 1581508800000: 100, 1581595200000: 14, 1581681600000: 93, 1581768000000: 24, 1581854400000: 48, 1581940800000: 33, 1582027200000: 5},
           filteredData: [],
           labels: [], //dayLabels
+          //daily_sums: this.props.data   
         };
       }
 
+
       filteredData = (startDateUnix) => {
-        let daily_sums = this.state.daily_sums;
-        // console.log(this.state.daily_sums);
+        let daily_sums = this.props.data; //this.state.daily_sums;
+        console.log("DATA BEING PASSED AS PROPS:")
+        console.log(daily_sums);
         let chartData = [];
         let charDays = [];
         let dayNames = dayLabels;
         let i = 0;
         let startDate = new Date(startDateUnix);
-         let checkDate = new Date(startDate);
-         let checkDateUnix = checkDate.getTime()
-         let endDate = new Date(startDateUnix);
-         endDate.setDate(startDate.getDate()+7);
-         let endDateUnix = endDate.getTime();
+        console.log("startDate" + startDate);
+        let checkDate = new Date(startDate);
+        let checkDateUnix = checkDate.getTime()
+        let endDate = new Date(); 
+        endDate.setUTCFullYear(startDate.getUTCFullYear());
+        endDate.setUTCMonth(startDate.getUTCMonth());
+        endDate.setUTCDate(startDate.getDate()+7);
+        endDate.setUTCHours(12);
+        endDate.setUTCMinutes(0);
+        endDate.setUTCSeconds(0);
+        endDate.setUTCMilliseconds(0);
+        console.log("endDate" + endDate);
+        let endDateUnix = endDate.getTime();
         while(checkDateUnix <= endDateUnix) {
             charDays[i] = dayNames[checkDate.getDay()] + " " + monthLabels[checkDate.getMonth()] + " " + checkDate.getDate() + ", " + checkDate.getFullYear();
+            console.log(checkDateUnix);
+            console.log(daily_sums);
+            console.log(daily_sums[checkDateUnix]);
+            console.log(checkDateUnix in daily_sums);
+            //console.log(daily_sums[1582459200000]);
+
             if (checkDateUnix in daily_sums) {
                 chartData[i] = daily_sums[checkDateUnix];
             }
@@ -42,16 +58,26 @@ export default class Dashboard extends Component {
             //console.log(checkDate.getTime());
             i++;
             // checkDate = checkDate.getDate()+1;
-            checkDate.setDate(checkDate.getDate() + 1);
+            checkDate.setDate(checkDate.getUTCDate() + 1);
             checkDateUnix = checkDate.getTime();
             console.log(checkDate)
             //console.log(chartData[0])
           }
+          console.log(chartData);
         this.setState({
             filteredData: chartData,
             labels: charDays
         })
     }
+
+    // componentDidUpdate = (prevProps, prevState) =>{
+    //   if(prevState.daily_sums !== this.state.daily_sums)
+    //   {
+        
+    //     this.filteredData(1582459200000);
+    //   }
+    //   console.log("Component did update!")
+    // }
 
     componentDidMount = () => {
         // let db = firebase.database();
@@ -65,13 +91,15 @@ export default class Dashboard extends Component {
         //   this.setState({ daily_sums: volume, filteredData: volume })
         // })
         console.log("hello");
-        this.filteredData(1581508800000);
+        
+        this.filteredData(1582459200000);
     }
 
 
 
     render() {
         // console.log(this.state.filteredData)
+        console.log(this.state.filteredData)
         const { filteredData, labels } = this.state;
         return (
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -13,6 +13,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faSignal, faClock, faLightbulb, faQuestionCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 import Dashboard from "../graphdata/Dashboard.js"
+import LineGraph from "../graphdata/myLineGraph.js";
+import classes from "./home.css";
+
 
 
 // Adds icons
@@ -35,24 +38,81 @@ class Welcome extends Component {
 
 console.log(logo);
 
+let dayLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthLabels = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       daily_sums: {}, //timestamp:sum
-      volume: 0
+      volume: 0,
+      filteredData: [],
+      labels: [], //dayLabels
     }
   }
-  getDaysData = (db) => {
 
-    let sumsRef = db.ref('daily_sums');
-    sumsRef.once('value').then(snapshot => {
-      let db_days = snapshot.val();
-      this.setState({ daily_sums: db_days });
+  // getDaysData = (db) => {
+
+  //   let sumsRef = db.ref('daily_sums');
+  //   sumsRef.once('value').then(snapshot => {
+  //     let db_days = snapshot.val();
+  //     this.setState({ daily_sums: db_days });
+  //   })
+  //   // this.setState({ daily_sums: sumsRef.once('value').snapshot.val()});
+  // }
+
+  filteredData = (startDateUnix) => {
+    let daily_sums = this.state.daily_sums;
+    console.log("DATA BEING PASSED AS PROPS:")
+    console.log(daily_sums);
+    let chartData = [];
+    let charDays = [];
+    let dayNames = dayLabels;
+    let i = 0;
+    let startDate = new Date(startDateUnix);
+    console.log("startDate" + startDate);
+    let checkDate = new Date(startDate);
+    let checkDateUnix = checkDate.getTime()
+    let endDate = new Date(); 
+    endDate.setUTCFullYear(startDate.getUTCFullYear());
+    endDate.setUTCMonth(startDate.getUTCMonth());
+    endDate.setUTCDate(startDate.getDate()+7);
+    endDate.setUTCHours(12);
+    endDate.setUTCMinutes(0);
+    endDate.setUTCSeconds(0);
+    endDate.setUTCMilliseconds(0);
+    console.log("endDate" + endDate);
+    let endDateUnix = endDate.getTime();
+    while(checkDateUnix <= endDateUnix) {
+        charDays[i] = dayNames[checkDate.getDay()] + " " + monthLabels[checkDate.getMonth()] + " " + checkDate.getDate() + ", " + checkDate.getFullYear();
+        console.log(checkDateUnix);
+        console.log(daily_sums);
+        console.log(daily_sums[checkDateUnix]);
+        console.log(checkDateUnix in daily_sums);
+        //console.log(daily_sums[1582459200000]);
+
+        if (checkDateUnix in daily_sums) {
+            chartData[i] = daily_sums[checkDateUnix];
+        }
+        else {
+            chartData[i] = 0;
+        }
+        //console.log(checkDate.getTime());
+        i++;
+        // checkDate = checkDate.getDate()+1;
+        checkDate.setDate(checkDate.getUTCDate() + 1);
+        checkDateUnix = checkDate.getTime();
+        console.log(checkDate)
+        //console.log(chartData[0])
+      }
+      console.log(chartData);
+    this.setState({
+        filteredData: chartData,
+        labels: charDays
     })
-    // this.setState({ daily_sums: sumsRef.once('value').snapshot.val()});
-  }
+}
 
   componentDidMount = () => {
     let db = firebase.database();
@@ -70,7 +130,7 @@ class Home extends Component {
       let noon_timestamp;
       let time_in_unix_ms;
       data.forEach(childSnapshot => {
-        timestamp = childSnapshot.key*1000;
+        timestamp = childSnapshot.key*1000; //s to ms
         //console.log(timestamp);
         vol = childSnapshot.child('volume').val();
 
@@ -78,8 +138,15 @@ class Home extends Component {
         date.setTime(timestamp);
 
         //Month: starting from 0 (Jan) -> ex. 4 is May
-        noon_timestamp = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0, 0);
-
+        //new Date set the time to PST -> 4 am PST -> 12 pm UTC
+        noon_timestamp = new Date();
+        noon_timestamp.setUTCFullYear(date.getUTCFullYear());
+        noon_timestamp.setUTCMonth(date.getUTCMonth());
+        noon_timestamp.setUTCDate(date.getUTCDate());
+        noon_timestamp.setUTCHours(12);
+        noon_timestamp.setUTCMinutes(0);
+        noon_timestamp.setUTCSeconds(0);
+        noon_timestamp.setUTCMilliseconds(0);
         //console.log(noon_timestamp.getHours());
 
         time_in_unix_ms = noon_timestamp.getTime()
@@ -93,13 +160,18 @@ class Home extends Component {
         console.log(daily_sums[time_in_unix_ms]);
 
       })
+
+      this.filteredData(1582459200000);
     })
 
     //this.setState({daily_sums:daily_sums});
   }
   render() {
     console.log(this.state.daily_sums);
+    const { filteredData, labels } = this.state;
+
     return (
+      
         <React.Fragment>
           
           <div className="body">
@@ -128,7 +200,16 @@ class Home extends Component {
                 <div className="body_box full_width double_height">
                   <div id="graph_wrap">
                     <div id="graph_htmlcontainer">
-                      <Dashboard/>
+                      
+                    <div className={classes.container}>
+                      <header>
+                        <h2>Your Usage History</h2>
+                      </header>
+                      <LineGraph
+                        data={filteredData}
+                        labels={labels} />
+                    </div>
+
                     </div>
                   </div>  
                 </div> 
