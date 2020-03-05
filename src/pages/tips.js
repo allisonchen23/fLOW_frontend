71f9a5028c27e024db38b324cbdf4ce742c32b78
Ray Huang <rayhuang.cj@gmail.com>
2020-03-04 22:56:54 -0800

    revert changes to tips

--- src/pages/tips.js
+++ src/pages/tips.js
@@ -1,9 +1,12 @@
-//Home Page
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from "react-router-dom";
 
 import './tips.css';
 import './globalstyle.css';
 import logo from '../assets/svg/flow_logo.svg';
+import dbman from '../assets/svg/dashboardimg.svg';
+import drop from '../assets/svg/drop.svg';
+import waterimage from '../assets/jpeg/waterimage.jpg';
 
 // Imports icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -66,6 +69,7 @@ class Tips extends Component {
               <div className="body_container">
                 <div className="body_box_t long2_t triple_height_t">
                   <div className="box_content_t" id="first_image">
+                    {/* <img className="dropimg" src={drop}/>              */}
                   </div>  
                 </div> 
                 <div className="body_box_t long2_t triple_height_t" id="message_box_t">
